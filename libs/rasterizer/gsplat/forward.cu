63bb3105c114c83c67a6c2f0528fa94bcf69b225
Murray Evans <me475@Murrays-MacBook-Air.local>
2026-06-26 12:03:06 +0100

    missed an all important line

--- libs/rasterizer/gsplat/forward.cu
+++ libs/rasterizer/gsplat/forward.cu
@@ -335,7 +335,7 @@ __global__ void nd_rasterize_forwardME(
         const float q = T  ; // for transmittance field.
         const float hdiff = q - colors[ channels * g + c ];
         const float hval  = vis * __expf( 2*q-2 ) * hdiff*hdiff;
-        
+        out_img[channels * pix_id + c ] += hval;
 
         // then we have sparsity
         ++c;
